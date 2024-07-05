4905508ee62efd00259e00b7ed2da19553ac309f
SarvarbekNematov <meoff1011@gmail.com>
2024-07-05 07:40:13 +0500

    dsa

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" >
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
